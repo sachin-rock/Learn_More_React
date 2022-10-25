7ccc7c74abf06872da25730e43f8183067786bcb
sacgin ingole <97524423+sachin-rock@users.noreply.github.com>
2022-10-25 10:48:52 +0530

    Done

---
+++ Product And Product DetailsPage/src/Components/ProductDetails.jsx
@@ -0,0 +1,34 @@
+
+// product details page
+
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const ProductDetails = () => {
+    const { id } = useParams();
+    const [product, setProduct] = useState([])
+
+
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/${id}`)
+            .then(res => res.json())
+            .then(res => setProduct(res))
+    }, [id])
+
+    console.log(product)
+
+    return (
+        <div>
+            <h1>{product.title}</h1>
+            <img style={{
+                width: "200px",
+                height: "200px"
+            }} src={product.image} alt="" />
+            <h3>{product.description}</h3>
+            <h3>{product.price}</h3>
+        </div>
+    )
+}
+
+export default ProductDetails
